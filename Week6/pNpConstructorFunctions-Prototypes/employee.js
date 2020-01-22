114632ea71fe1b05861e9f6e15f866d139708058
denaled <denaled@gmail.com>
2020-01-22 00:07:25 -0700

    FSW105 Constructors and Prototypes Commit1

--- Week6/pNpConstructorFunctions-Prototypes/employee.js
+++ Week6/pNpConstructorFunctions-Prototypes/employee.js
@@ -1,20 +1,32 @@
+
+var employees = []
+
 function Employee(name,jobTitle, salary, status){
     this.name = name
     this.jobTitle = jobTitle
     this.salary = salary
     this.status = status
-
+    this.changeStatus = function(statusChange){
+        this.status = statusChange
+    }
+    this.printEmployeeForm = function(person){
+        console.log("Name: " + person.name + " Job Title: " + person.jobTitle + " Salary: $" + person.salary + " Satus: " + person.status)
+    }
 }
+var employee1 = new Employee("Dan", "Janitor", "35k", "Fulltime")
+var employee2 = new Employee("Steve", "President", "100k", "Fulltime")
+var employee3 = new Employee("Suzy", "Bookeeper", "45k" , "Fulltime")
+//Override the status attribute of one of them to either "Part Time" or "Contract"
+employee2.changeStatus("Part-Time")
 
-var employees = [
-    ["Dan", "Janitor", "35k", "fulltime"],
-    ["Steve", "President", "100k", "fulltime"],
-    ["Suzy", "Bookeeper", "45k" , "fulltime"]
-]
-var employee = []
+employee1.printEmployeeForm(employee1)
+employee2.printEmployeeForm(employee2)
+employee3.printEmployeeForm(employee3)
 
-for(i=0; i < employees.length ; ++i){
-    employee.push = new Employee(employees[i])
-
+function addEmployee(passEmployee){
+    employees.push(passEmployee)
 }
-console.log(employee.length)
+addEmployee(employee1)
+addEmployee(employee2)
+addEmployee(employee3)
+console.log(employees)
